6221d051253512e597b2c46ce43246c56c29487d
smlrkh1107 <krae7789230@gmail.com>
2020-04-12 18:26:20 +0900

    마이페이지로 넘어가기 및 전체적 앱 색상 변경, 앱 타이틀 적어주기

--- app/src/main/java/com/example/intentpractice_address/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_address/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.intentpractice_address
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goMyPageBtn.setOnClickListener {
+
+            val myIntent = Intent(this,MyPageActivity::class.java)
+            startActivity(myIntent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/example/intentpractice_address/MyPageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.intentpractice_address
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
